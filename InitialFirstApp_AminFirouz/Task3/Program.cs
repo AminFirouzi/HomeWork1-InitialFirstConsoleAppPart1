499cc07ba9797ba0657702f44711717ac09baeb8
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2026-06-12 15:01:08 +0330

    all done

---
+++ InitialFirstApp_AminFirouz/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//amin firouzi bandpey
+
+Random random = new Random();
+int trueNumber = random.Next(1, 11);
+int counter = 5;
+while (counter >= 1)
+{
+    Console.WriteLine("enter you guess(1, 10): ");
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int userGuessNumber) && (userGuessNumber >= 0 && userGuessNumber <= 10))
+    {
+        if (userGuessNumber == trueNumber)
+        {
+            Console.WriteLine("horay, you guessed the number, its {0}", trueNumber);
+            break;
+        }
+        else
+        {
+            if (counter - 1 == 0)
+            {
+                Console.WriteLine("GAME OVER!!!, your out of guess!");
+            }
+            else
+            {
+                Console.WriteLine("oops, wrong number! you have {0} remaining guesses", counter - 1);
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("invalid input! not a number or out of bound");
+    }
+    counter--;
+}
---
+++ InitialFirstApp_AminFirouz/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool IsOdd(int number) => number % 2 == 1;
+
+int SumOfAllOdds(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i ++)
+        if (IsOdd(i))
+            sum += i;
+    
+    return sum;
+}
+
+Console.WriteLine("enter a number(greater than 1):");
+
+string userInput = Console.ReadLine();
+if (int.TryParse(userInput, out int number))
+{
+    if(number <  1)
+        Console.WriteLine("i said greater than 1!!!!");
+    else
+    {
+        for(int i = 1; i <= number; i++)
+            if(IsOdd(i))
+                Console.WriteLine(i);
+        Console.WriteLine("sum of all odd number: {0}", SumOfAllOdds(number));
+
+        
+    }
+}
+else
+{
+    Console.WriteLine("invalid input");
+}
